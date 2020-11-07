848abe263cbedf9d9144f3f70b12f5c6158d5ebf
Shambhavi Ramaswamy <55058421+shambhavir@users.noreply.github.com>
2020-11-06 22:58:46 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
-import { NEWS_API_KEY } from "./config";
-
-
+import { NEWS_API_KEY } from "./config"; //our Business API key imported from the config file 
+//separate calls to each cateogry that are called in different components for respective topics of tech, sports, and entertainment 
 export const getEntertainment = async topic => {
   const response = await fetch(
     `https://newsapi.org/v2/top-headlines?country=us&category=entertainment&pageSize=100&apiKey=${NEWS_API_KEY}`
