ef14a89377c5dd4f7d37495e48327e95d3bdd5fb
Rams Ramaswamy <aramaswamy@Ramss-iMac.local>
2020-11-06 22:29:36 -0500

    changed footer sizing

--- src/footer.js
+++ src/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./footer.css"; 
 const footer = () => (
   <div className="footer">
-       <p style={{ textAlign: "center", fontSize:"30px" }}>
+       <p style={{ textAlign: "center", fontSize:"25px" }}>
        Made with ❤️ and ☕️ by <a href="https://shambhavir.github.io/">Shambhavi Ramaswamy.</a>
         </p>
   </div>
